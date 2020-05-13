42568aa4dac4147be95a323ae731182f08ecc2b8
bmagee2 <bamagee2@gmail.com>
2020-05-13 10:06:38 -0400

    edits

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const baseURL = "http://openlibrary.org/search.json?q=";
 
 let url;
+
+const searchTerm = document.querySelector('.search');
+const searchForm = document.querySelector('form');                  
+const submitBtn = document.querySelector('.submit');
+
+const nextBtn = document.querySelector('.next');
+const previousBtn = document.querySelector('.prev');
+const nav = document.querySelector('nav');
+
+const section = document.querySelector('section');
+
+
+nav.style.display = 'none';
+
+
+let pageNumber = 0;
+let displayNav = false; 
+
+
+searchForm.addEventListener('submit', fetchResults);
+nextBtn.addEventListener('click', nextPage);            // (3)
+previousBtn.addEventListener('click', previousPage);
